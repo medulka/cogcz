b4e5a588ebd083924e67c31a1c243a02ba42998f
Lucie Pfeiferová <luciepfeiferova@Lucie-MacBook-Pro.local>
2021-10-21 12:27:07 +0200

    week 40 version 1

--- template_main.py
+++ template_main.py
@@ -19,15 +19,15 @@ import base64
 
 #update regularly
 #change in the reporting since 16.7.2021 (week28) - only last 12 months
-DF_DATE = "24.9.2021"
-ACTUAL_SAMPLE_SIZE = "2 219"
+DF_DATE = "08.10.2021"
+ACTUAL_SAMPLE_SIZE = "2 585"
 LINEAGE_COUNTS = "31"
-WEEK = "38"
-STARTING_WEEK = "27"
-SEQUENCED_RATIO = "11,37"
+WEEK = "40"
+STARTING_WEEK = "29"
+SEQUENCED_RATIO = "10.19"
 MUTATIONS_COUNT = "1 700"
 NOW=datetime.datetime.now().strftime('%d. %m. %Y' )
-VOC = ['B.1.351', 'P.1', 'B.1.617.2','B.1.1.7',"B.1.351.2","B.1.351.5", "AY.1", "AY.2","AY.3", "AY.4","AY.4.1", "AY.5", "AY.5.2", "AY.6","AY.7.1", "AY.7.2", "AY.9","AY.10","AY.11", "AY.12", "AY.16", "AY.20", "AY.21","AY.23","AY.26","AY.27","AY.31","AY.32","AY.33","AY.34","AY.36","AY.37" ]
+VOC = ['B.1.351', 'P.1', 'B.1.617.2','B.1.1.7',"B.1.351.2","B.1.351.5", "AY.1", "AY.2","AY.3", "AY.4","AY.4.1", "AY.5", "AY.5.2", "AY.6","AY.7","AY.7.1", "AY.7.2", "AY.9","AY.10","AY.11", "AY.12", "AY.16", "AY.20", "AY.21","AY.23","AY.25","AY.26","AY.27","AY.31","AY.32","AY.33","AY.34","AY.36","AY.37" ]
 VOI = ['B.1.620', 'B.1.621', "C.37" ]
 # uprav tyden v range()
 
